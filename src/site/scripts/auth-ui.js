ec0ee2aa558ce72bb191a74b12476cde0b6cd7c5
Rubén Rivera <rr@rubenrivera.mx>
2024-02-23 16:05:47 -0600

    Update auth-ui.js

--- src/site/scripts/auth-ui.js
+++ src/site/scripts/auth-ui.js
@@ -1,8 +1,6 @@
 // URL mapping, from hash to a function that responds to that URL action
 const router = {
   "/": () => showContent("content-home"),
-  "/profile": () =>
-    requireAuth(() => showContent("content-profile"), "/profile"),
   "/login": () => login()
 };
 
