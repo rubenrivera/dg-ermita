292abefbb0e98cde0cbd3c5b44ea56656cbc0bc5
Rubén Rivera <rr@rubenrivera.mx>
2024-02-23 18:38:32 -0600

    Update auth.js

--- src/site/scripts/auth.js
+++ src/site/scripts/auth.js
@@ -4,7 +4,7 @@ let auth0Client = null;
 /**
  * Starts the authentication flow
  */
-const login = async (targetUrl = window.location.href) => {
+const login = async (targetUrl) => {
   try {
     console.log("Logging in", targetUrl);
 
